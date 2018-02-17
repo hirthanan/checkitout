c0330838bbcb7b9f718b312926444974d1056939
Hirthanan Subenderan <Hirthanan_97@hotmail.com>
2018-02-16 23:43:49 -0500

    restructured code and improved readability

--- bot/footlocker/footlocker.py
+++ bot/footlocker/footlocker.py
@@ -6,6 +6,7 @@ class FootlockerShop:
     def __init__(self):
         print("FootlockerShop Initialized")
         print("While running")
+        self.request_key = None
 
 
     '''URL only needs a sku number for product of interest
@@ -15,12 +16,8 @@ class FootlockerShop:
         return url
 
 
-    def checkout(self, sku):
-        print("sku = " + str(sku))
-
-
     ''' returns chosen items associated sku-number '''
-    def chooseItem(self, items):
+    def getSku(self, items):
         print("Verify the item number that you were interested in automagically buying")
         itemArr = list(items.keys())
         for idx, item in enumerate(itemArr):
@@ -40,7 +37,7 @@ class FootlockerShop:
 
 
 
-    def getItems(self, soup, item):
+    def getReleaseItems(self, soup, item):
         items = {} # each item will be formatted, {'name': {'sku':[vals], 'color':[vals]}}
 
         potential_items = soup.find_all('div', title=lambda x: x and item in x)
@@ -65,7 +62,8 @@ class FootlockerShop:
         return items
 
 
-
+    ''' Looks through release calendar of all items and
+    finds the exact SKU (id) number of your inputted item '''
     def releaseCalendar(self):
         newly_released_url = "http://www.footlocker.ca/en-CA/releasecalendarca"
 
@@ -77,20 +75,39 @@ class FootlockerShop:
             html = browser.page_source
             soup = bs(html, "lxml")
 
-            items = self.getItems(soup, 'Jordan')
-            # item = self.chooseItem(items)
+            # print soup.prettify()
+            # self.release_key = soup.find('input', {'id': 'requestKey'})['value']
+            # print self.release_key
+
+            items = self.getReleaseItems(soup, 'Jordan')
+            sku = self.getSku(items)
         except Exception, e:
             print("ERROR: " + str(e))
 
         browser.quit()
 
-        return soup
+        return sku
 
+    def addCart(self, sku):
+        addToCartUrl = 'http://www.footlocker.ca/catalog/addToCart'
 
-    def main(self):
         # headers = requests.utils.default_headers()
         # headers.update({'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'})
 
-        releases = self.releaseCalendar()
+
+
+
+
+    def main(self):
+
+        itemSku = self.releaseCalendar()
+        success = self.addCart(itemSku)
+
+        if success:
+            print ("Successfully added your item to your Cart")
+        else:
+            print ("Failed to add your item to your Cart")
+
+
 
 

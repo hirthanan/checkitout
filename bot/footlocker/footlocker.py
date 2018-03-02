f4eb652ffb75d41316555370bfaba9a600d2cace
Hirthanan Subenderan <Hirthanan_97@hotmail.com>
2018-03-01 23:37:29 -0500

    added all data and user input for adding to checkout

--- bot/footlocker/footlocker.py
+++ bot/footlocker/footlocker.py
@@ -1,5 +1,10 @@
-import re
+# requests library used to create api requests
+import requests
+
+# selenium webdriver used to retrieve dynamic data from page source
 from selenium import webdriver
+
+# beatifulsoup used to parse html
 from bs4 import BeautifulSoup as bs
 
 
@@ -8,14 +13,14 @@ class FootlockerShop:
     def __init__(self):
         print("FootlockerShop Initialized")
         print("While running")
-        self.request_key = None
         self.browser = None
+        self.shoeSize = None
 
 
     '''URL only needs a sku number for product of interest
        which can be retrieved from html markup on footlockers newly released calendar'''
-    def genUrl(self, sku):
-        url = "http://www.footlocker.ca/en-CA/product/sku:" + str(sku)
+    def genUrl(self, sku, model):
+        url = "http://www.footlocker.ca/en-CA/product/model:" + str(model) + "/sku:" + str(sku);
         return url
 
     ''' Using regex, finds the requestKey in html '''
@@ -35,23 +40,28 @@ class FootlockerShop:
 
         return reqKey
 
-    ''' returns chosen items associated sku-number '''
-    def getSku(self, items):
-        print("Verify the item number that you were interested in automagically buying")
+    ''' returns chosen items associated properties '''
+    def getItem(self, items):
+        print("Verify the item number that you were interested in automagically checking")
         itemArr = list(items.keys())
         for idx, item in enumerate(itemArr):
             print(str(idx+1) + ". " + item)
         num = input("# => ")
         verified_name = itemArr[num-1]
 
-        print("Verify the color of the item that you were interested in automagically buying")
+        print("Verify the color of the item that you were interested in automagically checking")
         colorArr = items[verified_name]['color']
         for idx, color in enumerate(colorArr):
             print(str(idx+1) + ". " + color)
         num = input("# => ")
+        print("What is your Shoe Size? (i.e 09.0)")
+        self.shoeSize = input("# => ")
         verified_sku = items[verified_name]['sku'][num-1]
+        verified_model = items[verified_name]['model_num'][num-1]
 
-        return verified_sku
+        props = {'sku':verified_sku, 'model_num':verified_model}
+
+        return props
 
 
 
@@ -66,16 +76,18 @@ class FootlockerShop:
             productName = item.findAll(attrs={'class' : 'productName'})
             item_name = str(productName[0].contents[0].strip())
 
-            item_sku = int(item["data-skunumber"])
+            item_sku = str(item["data-skunumber"])
+            item_model = str(item["data-modelnumber"])
 
             colorway = item.findAll(attrs={'class' : 'colorway'})
             item_color = str(colorway[0].contents[0].strip())
 
             if item_name not in items:
-                items[item_name] = {'sku':[item_sku], 'color':[item_color]}
+                items[item_name] = {'sku':[item_sku], 'color':[item_color], 'model_num':[item_model]}
             else:
                 items[item_name]['sku'].append(item_sku)
                 items[item_name]['color'].append(item_color)
+                items[item_name]['model_num'].append(item_model)
 
         return items
 
@@ -97,40 +109,54 @@ class FootlockerShop:
         soup = self.soupify(newly_released_url)
 
         items = self.getReleaseItems(soup, 'Jordan')
-        sku = self.getSku(items)
+        item = self.getItem(items)
 
-        return sku
+        return item
 
-    def addCart(self, sku):
+    def addCart(self, item):
         addToCartUrl = 'http://www.footlocker.ca/catalog/addToCart'
-        productUrl = self.genUrl(sku)
+        sku = item['sku']
+        model = item['model_num']
+        productUrl = self.genUrl(sku, model)
 
         soup = self.soupify(productUrl)
         requestKey = self.getRequestKey(soup)
 
-        print requestKey
+        headers = {
+                    'Accept': '*/*',
+                    'Accept-Encoding': 'gzip, deflate',
+                    'Accept-Language': 'en-US,en;q=0.5',
+                    'Connection': 'keep-alive',
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    'Host': 'www.footlocker.ca',
+                    'Referer': productUrl
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36',
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+
+        payload = {
+            'requestKey': requestKey,
+            'qty':1,
+            'size':09.0,
+            'the_model_nbr':model,
+            'sku':sku,
+            'storeNumber':00000,
+            'fulfillmentType':'SHIP_FROM_STORE',
+            'storeCostOfGoods':0.00,
+            'inlineAddToCart':0,
+            'coreMetricsCategory':'blank',
+            'pdp_addtocart':'Add To Cart'
+        }
 
 
-        # headers = requests.utils.default_headers()
-        # headers.update({'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'})
 
-        # payload = {
-        #     "Host": "www.mywbsite.fr",
-        #     "Connection": "keep-alive",
-        #     "Content-Length": 129,
-        #     "Origin": "https://www.mywbsite.fr",
-        #     "X-Requested-With": "XMLHttpRequest",
-        #     "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5",
-        #     "Content-Type": "application/json",
-        #     "Accept": "*/*",
-        #     "Referer": "https://www.mywbsite.fr/data/mult.aspx",
-        #     "Accept-Encoding": "gzip,deflate,sdch",
-        #     "Accept-Language": "fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4",
-        #     "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
-        #     "Cookie": "ASP.NET_SessionId=j1r1b2a2v2w245; GSFV=FirstVisit=; GSRef=https://www.google.fr/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&ved=0CHgQFjAA&url=https://www.mywbsite.fr/&ei=FZq_T4abNcak0QWZ0vnWCg&usg=AFQjCNHq90dwj5RiEfr1Pw; HelpRotatorCookie=HelpLayerWasSeen=0; NSC_GSPOUGS!TTM=ffffffff09f4f58455e445a4a423660; GS=Site=frfr; __utma=1.219229010.1337956889.1337956889.1337958824.2; __utmb=1.1.10.1337958824; __utmc=1; __utmz=1.1337956889.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)"
-        # }
+        session = requests.Session()
 
+        status = session.post(addToCartUrl, headers=headers, data=payload)
 
+        if status:
+            return True
+        return False
 
 
     def main(self):
@@ -138,8 +164,8 @@ class FootlockerShop:
         try:
             self.browser = webdriver.Firefox()
 
-            itemSku = self.releaseCalendar()
-            success = self.addCart(itemSku)
+            item = self.releaseCalendar()
+            success = self.addCart(item)
 
             if success:
                 print ("Successfully added your item to your Cart")
@@ -149,7 +175,7 @@ class FootlockerShop:
         except Exception, e:
             print("ERROR: " + str(e))
 
-        self.browser.quit()
+        # self.browser.quit()
 
 
 
